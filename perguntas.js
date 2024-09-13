c3ce444e710ca70ef2135ebca182f8fe091799c4
MatheusAS2189 <145040983+MatheusAS2189@users.noreply.github.com>
2024-09-13 10:44:56 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,7 +1,7 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'O que é Java Script?',
+    'O Java Script é uma linguagem de programação'
 )
 
 criaCartao(
@@ -11,13 +11,13 @@ criaCartao(
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Matematica',
+    'Quanto é 2+2x4?',
+    '2+2x4 é igual a 10'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Lingua Espanhola',
+    'Como se diz oi em Espanhol?',
+    'Oi em Espanhol é HOLA'
 )
